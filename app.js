e6165ce0f96b68df2c5c63a50082698087f1e182
Le Ba Luong <lebaluong33@gmail.com>
2021-09-26 09:12:40 +0700

    add month and year milestone

--- app.js
+++ app.js
@@ -1,60 +1,67 @@
-const yourDate = new Date('Wed Dec 02 2020 00:00:00 GMT+0700');
+const myDate = new Date("Wed Dec 02 2020 00:00:00 GMT+0700");
 const songName = {
-  'i-miss-you': '[Goblin OST] Soyou - I Miss You (FMV) [HAN-ROM-ENG]',
-  everytime: '[MV] CHEN(첸)XPunch(펀치) - Everytime l 태양의 후예 OST Part.2',
-  'this-love':
-    '[MV] DAVICHI(다비치) - This Love(이 사랑) l 태양의 후예 OST Part.3',
-  'beautiful-in-white': 'Westlife - Beautiful in white',
-  'you-are-my-everything': 'You Are My Everything',
-  'Doi-Loi-Hoang-Dung': 'Đôi lời - Hoàng Dũng',
-  'Meghan-Trainor-Like-Im-Gonna-Lose-You-ft-JohnLegend':
-    'Meghan Trainor Like Im Gonna Lose You ft JohnLegend',
-  'vi-yeu-la-nho': 'Vì yêu là nhớ',
-  'tu-thich-thich-thanh-thuong-thuong': 'Từ thích thích thành thương thương',
-  'the-playah': 'SOOBIN X SLIMV - THE PLAYAH (Special Performance)',
-  'khong-sao-ma-em-day-roi':
-    'KHÔNG SAO MÀ EM ĐÂY RỒI - SUNI HẠ LINH ft. Lou Hoàng',
+  "i-miss-you": "[Goblin OST] Soyou - I Miss You (FMV) [HAN-ROM-ENG]",
+  everytime: "[MV] CHEN(첸)XPunch(펀치) - Everytime l 태양의 후예 OST Part.2",
+  "this-love": "[MV] DAVICHI(다비치) - This Love(이 사랑) l 태양의 후예 OST Part.3",
+  "beautiful-in-white": "Westlife - Beautiful in white",
+  "you-are-my-everything": "You Are My Everything",
+  "Doi-Loi-Hoang-Dung": "Đôi lời - Hoàng Dũng",
+  "Meghan-Trainor-Like-Im-Gonna-Lose-You-ft-JohnLegend":
+    "Meghan Trainor Like Im Gonna Lose You ft JohnLegend",
+  "vi-yeu-la-nho": "Vì yêu là nhớ",
+  "tu-thich-thich-thanh-thuong-thuong": "Từ thích thích thành thương thương",
+  "the-playah": "SOOBIN X SLIMV - THE PLAYAH (Special Performance)",
+  "khong-sao-ma-em-day-roi": "KHÔNG SAO MÀ EM ĐÂY RỒI - SUNI HẠ LINH ft. Lou Hoàng",
 };
 
 const music = [
-  'i-miss-you',
-  'everytime',
-  'this-love',
-  'beautiful-in-white',
-  'you-are-my-everything',
-  'Doi-Loi-Hoang-Dung',
-  'Meghan-Trainor-Like-Im-Gonna-Lose-You-ft-JohnLegend',
-  'vi-yeu-la-nho',
-  'tu-thich-thich-thanh-thuong-thuong',
-  'the-playah',
-  'khong-sao-ma-em-day-roi',
+  "i-miss-you",
+  "everytime",
+  "this-love",
+  "beautiful-in-white",
+  "you-are-my-everything",
+  "Doi-Loi-Hoang-Dung",
+  "Meghan-Trainor-Like-Im-Gonna-Lose-You-ft-JohnLegend",
+  "vi-yeu-la-nho",
+  "tu-thich-thich-thanh-thuong-thuong",
+  "the-playah",
+  "khong-sao-ma-em-day-roi",
 ];
 
-document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    const rootTime = document.querySelector('time');
-    const audio = document.getElementById('audio');
-    document.querySelector('anni').textContent = `${
-      yourDate.getDate() > 9 ? yourDate.getDate() : '0' + yourDate.getDate()
-    }-${
-      yourDate.getMonth() > 8
-        ? yourDate.getMonth() + 1
-        : '0' + (yourDate.getMonth() + 1)
-    }-${yourDate.getFullYear()}`;
+function monthDiff(d1) {
+  let d2 = new Date();
+  let months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+}
 
-    document.querySelector('date').textContent =
-      Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) +
-      ' DAYS';
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const rootTime = document.querySelector("time");
+    const audio = document.getElementById("audio");
+    document.querySelector("anni").textContent = `${
+      myDate.getDate() > 9 ? myDate.getDate() : "0" + myDate.getDate()
+    }-${
+      myDate.getMonth() > 8 ? myDate.getMonth() + 1 : "0" + (myDate.getMonth() + 1)
+    }-${myDate.getFullYear()}`;
+
+    const dayN = Math.floor(Math.floor((new Date() - myDate) / 1000) / 60 / 60 / 24) + " Days";
+    const monthN = monthDiff(myDate) + " Months";
+    const yearN = Math.floor(monthDiff(myDate) / 12);
+    const eventContent = yearN ? [dayN, monthN, yearN + " Years"] : [dayN, monthN];
+    document.querySelector("date").textContent = eventContent.join(" - ");
 
     function olock() {
       var today = new Date(),
-        hrs = Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60) % 24,
-        min = Math.floor(Math.floor((today - yourDate) / 1000) / 60) % 60,
-        sec = Math.floor((today - yourDate) / 1000) % 60;
-      rootTime.textContent = `${hrs > 9 ? hrs : '0' + hrs}:${
-        min > 9 ? min : '0' + min
-      }:${sec > 9 ? sec : '0' + sec}`;
+        hrs = Math.floor(Math.floor((today - myDate) / 1000) / 60 / 60) % 24,
+        min = Math.floor(Math.floor((today - myDate) / 1000) / 60) % 60,
+        sec = Math.floor((today - myDate) / 1000) % 60;
+      rootTime.textContent = `${hrs > 9 ? hrs : "0" + hrs}:${min > 9 ? min : "0" + min}:${
+        sec > 9 ? sec : "0" + sec
+      }`;
     }
     setInterval(function () {
       olock();
@@ -65,30 +72,24 @@ document.addEventListener(
       currentSongEl.scrollIntoView();
     };
     const play = () => {
-      audio.setAttribute('src', `music/${music[currentSong]}.mp3`);
-      let htmlRender = '';
+      audio.setAttribute("src", `music/${music[currentSong]}.mp3`);
+      let htmlRender = "";
       Object.values(songName).map((item, index) => {
         htmlRender += `<div class="song-name-container">
-        ${
-          index === currentSong
-            ? `<img id="dvd-icon-spin" src="./img/couple.jpg"/>`
-            : ''
-        }
+        ${index === currentSong ? `<img id="dvd-icon-spin" src="./img/couple.jpg"/>` : ""}
             <p id=${music[index]}>
               ${index === currentSong ? `<strong>${item}</strong>` : item}
             </p>
         </div>
         `;
       });
-      document.getElementById(
-        'song-name'
-      ).innerHTML = `<div>${htmlRender}<div/>`;
+      document.getElementById("song-name").innerHTML = `<div>${htmlRender}<div/>`;
       songName[music[currentSong]];
       scrollToSong();
       olock();
 
       music.map((item, index) => {
-        document.getElementById(item).addEventListener('dblclick', function () {
+        document.getElementById(item).addEventListener("dblclick", function () {
           currentSong = index;
           play();
         });
@@ -99,34 +100,30 @@ document.addEventListener(
       currentSong = currentSong === music.length - 1 ? 0 : currentSong + 1;
       play();
     };
-    document
-      .getElementById('btn-next')
-      .addEventListener('click', () => playNext());
+    document.getElementById("btn-next").addEventListener("click", () => playNext());
     const playPrev = () => {
       currentSong = currentSong ? currentSong - 1 : music.length - 1;
       play();
     };
-    document
-      .getElementById('btn-prev')
-      .addEventListener('click', () => playPrev());
+    document.getElementById("btn-prev").addEventListener("click", () => playPrev());
     audio.onended = () => {
       playNext();
     };
     document
-      .getElementsByTagName('body')[0]
-      .insertAdjacentHTML('beforeend', "<div id='mask'></div>");
+      .getElementsByTagName("body")[0]
+      .insertAdjacentHTML("beforeend", "<div id='mask'></div>");
   },
   false
 );
 
-var container = document.getElementById('heart'),
-  bigHeart = document.getElementById('bigHeart'),
-  shadow = document.getElementById('shadow'),
-  heartSrc = bigHeart.getAttribute('src'),
-  A = document.createElement('audio');
-A.src = './sounds/heart-click.mp3';
+var container = document.getElementById("heart"),
+  bigHeart = document.getElementById("bigHeart"),
+  shadow = document.getElementById("shadow"),
+  heartSrc = bigHeart.getAttribute("src"),
+  A = document.createElement("audio");
+A.src = "./sounds/heart-click.mp3";
 A.volume = 0.7;
-bigHeart.addEventListener('click', generateHeart);
+bigHeart.addEventListener("click", generateHeart);
 function generateHeart() {
   A.currentTime = 0.09;
   A.play();
@@ -136,19 +133,19 @@ function generateHeart() {
     { scale: 1 },
     { scale: 0.88, repeat: 1, yoyo: true, ease: Back.easeIn.config(7) }
   );
-  var newH = document.createElement('img'),
-    newC = document.createElement('div'),
+  var newH = document.createElement("img"),
+    newC = document.createElement("div"),
     WH = R(80, 15),
     newDs = [];
   for (var nd, i = 2; i--; ) {
-    nd = document.createElement('div');
-    nd.className = 'dots';
+    nd = document.createElement("div");
+    nd.className = "dots";
     newDs.push(nd);
     container.insertBefore(nd, bigHeart);
   }
   newH.src = heartSrc;
-  newH.className = 'hearts';
-  newC.className = 'circ';
+  newH.className = "hearts";
+  newC.className = "circ";
   TweenLite.set(newH, { width: WH, height: WH });
   container.insertBefore(newH, bigHeart);
   container.appendChild(newC);
@@ -175,12 +172,7 @@ function heartAnim(e1, e2, e34) {
     opacity: 1,
     ease: SlowMo.ease.config(0.1, 0.8, true),
   });
-  TweenLite.fromTo(
-    e2,
-    0.6,
-    { force3D: true, scale: 0.3 },
-    { scale: 1, opacity: 0 }
-  );
+  TweenLite.fromTo(e2, 0.6, { force3D: true, scale: 0.3 }, { scale: 1, opacity: 0 });
   TweenMax.staggerTo(
     e34,
     R(3.5, 1.5),
